1fa0358c8810ee2fdd6a6716b83a27d443d14a9f
Maxim Lukoshko <maxim.lukoshko@effective-soft.com>
2020-03-09 20:42:50 +0300

    Installed NUnit

---
+++ SightlyTestTask/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿
+namespace SightlyTestTask
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using NUnit.Framework;
+
+    using Assert = NUnit.Framework.Assert;
+
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestCase(1)]
+        [TestCase(2)]
+        public void TestMethod1(int a)
+        {
+            Assert.AreEqual(1, a);
+        }
+    }
+}
