594cde3e360f75f1bd873a1d2baa071676ad3f6f
Maxim Lukoshko <maxim.lukoshko@effective-soft.com>
2020-03-09 21:10:25 +0300

    Installed WebDriver

--- SightlyTestTask/UnitTest1.cs
+++ SightlyTestTask/UnitTest1.cs
@@ -1,20 +1,42 @@
 ﻿
 namespace SightlyTestTask
 {
-    using Microsoft.VisualStudio.TestTools.UnitTesting;
-
     using NUnit.Framework;
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Chrome;
+    using OpenQA.Selenium.Support.UI;
 
     using Assert = NUnit.Framework.Assert;
 
-    [TestClass]
+    [TestFixture]
     public class UnitTest1
     {
-        [TestCase(1)]
-        [TestCase(2)]
-        public void TestMethod1(int a)
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetupTest()
         {
-            Assert.AreEqual(1, a);
+            this.driver = new ChromeDriver();
+        }
+
+        [Test(Description = "Check SauceLabs Homepage for Login Link")]
+        public void TestMethod()
+        {
+            var homeURL = "https://www.SauceLabs.com";
+            this.driver.Navigate().GoToUrl(homeURL);
+
+            var wait = new WebDriverWait(this.driver, System.TimeSpan.FromSeconds(15));
+            wait.Until(dr => dr.FindElement(By.XPath("//a[@href='/beta/login']")));
+
+            var element = this.driver.FindElement(By.XPath("//a[@href='/beta/login']"));
+
+            Assert.AreEqual("Sign In", element.GetAttribute("text"));
+        }
+
+        [TearDown]
+        public void TearDownTest()
+        {
+            driver.Close();
         }
     }
 }
