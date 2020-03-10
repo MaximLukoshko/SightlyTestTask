d212c091f250f8758a3733267205888e4ec8b3bc
Maxim Lukoshko <maxim.lukoshko@effective-soft.com>
2020-03-10 17:27:54 +0300

    Clicked to report downloading

--- SightlyTestTask/SightlyClient.cs
+++ SightlyTestTask/SightlyClient.cs
@@ -1,8 +1,8 @@
 ﻿namespace SightlyTestTask
 {
     using System;
+    using System.Threading;
 
-    using OpenQA.Selenium;
     using OpenQA.Selenium.Chrome;
     using OpenQA.Selenium.Support.UI;
 
@@ -19,22 +19,53 @@
                 driver.Navigate().GoToUrl(homeURL);
 
                 var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
-                wait.Until(dr => dr.FindElement(By.XPath("//input[@placeholder='Your email address']")));
+                wait.Until(dr => dr.GetInputByPlaceHolder("Your email address"));
 
-                var emailElement = driver.FindElement(By.XPath("//input[@placeholder='Your email address']"));
-                //emailElement.Click();
+                var emailElement = driver.GetInputByPlaceHolder("Your email address");
                 emailElement.Clear();
                 emailElement.SendKeys("nick@sightly.com");
 
-                var passwordElement = driver.FindElement(By.XPath("//input[@placeholder='Your password']"));
+                var passwordElement = driver.GetInputByPlaceHolder("Your password");
                 passwordElement.Clear();
-                //passwordElement.Click();
                 passwordElement.SendKeys("a");
 
-                var loginButton = driver.FindElement(By.XPath("//button[contains(@class, 'login-button')]"));
-                loginButton.Click();
+                var loginButton = driver.GetButtonByClass("login-button");
+                loginButton.Click();               
+                wait.Until(dr => dr.GetDivById("header-reports"));
 
-                wait.Until(dr => dr.FindElement(By.XPath("//input[@placeholder='re']")));
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+
+                var reportsDiv = driver.GetDivById("header-reports");
+                reportsDiv.Click();
+
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+
+                var firstRowCheckBox = driver.GetFirstCheckBoxInTable("checkbox-style");
+                firstRowCheckBox.Click();
+
+                Thread.Sleep(TimeSpan.FromSeconds(3));
+
+                var createReportButton = driver.GetButtonByClass("create-report");
+                createReportButton.Click();
+
+                Thread.Sleep(TimeSpan.FromSeconds(5));
+
+                var radioButton = driver.GetRadioButtonByValue("performanceDetail");
+                radioButton.Click();
+
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+                
+                var costBasis = driver.GetSelectDropDownByClass("report-dialog-select", 2);
+                costBasis.SelectByValue("all");
+
+                var timePeriod = driver.GetSelectDropDownByClass("report-dialog-select", 2);
+                timePeriod.SelectByValue("all");
+
+                Thread.Sleep(TimeSpan.FromSeconds(1));
+
+                var runReportButton = driver.GetButtonByClass("run-report-button");
+                runReportButton.Click();
+                Thread.Sleep(TimeSpan.FromSeconds(10));
 
                 result =
                     @"C:\Users\maxim.lukoshko\Downloads\PerformanceDetail-Campaign-A48EB038-5D47-40F8-A2BD-E897AEAC4A9F - Copy2.xlsx";
---
+++ SightlyTestTask/WebDriverExtensions.cs
@@ -0,0 +1,38 @@
+﻿namespace SightlyTestTask
+{
+    using OpenQA.Selenium;
+    using OpenQA.Selenium.Support.UI;
+
+    public static class WebDriverExtensions
+    {
+        public static IWebElement GetInputByPlaceHolder(this IWebDriver webDriver, string placeHolder)
+        {
+            return webDriver.FindElement(By.XPath($"//input[@placeholder='{placeHolder}']"));
+        }
+
+        public static IWebElement GetButtonByClass(this IWebDriver webDriver, string buttonClass)
+        {
+            return webDriver.FindElement(By.XPath($"//button[contains(@class, '{buttonClass}')]"));
+        }
+
+        public static IWebElement GetDivById(this IWebDriver webDriver, string divId)
+        {
+            return webDriver.FindElement(By.XPath($"//div[@id='{divId}']"));
+        }
+
+        public static IWebElement GetFirstCheckBoxInTable(this IWebDriver webDriver, string divClass)
+        {
+            return webDriver.FindElement(By.XPath($"(//input[contains(@class, '{divClass}')])[2]"));
+        }
+
+        public static IWebElement GetRadioButtonByValue(this IWebDriver webDriver, string value)
+        {
+            return webDriver.FindElement(By.XPath($"//input[@value='{value}']"));
+        }
+
+        public static SelectElement GetSelectDropDownByClass(this IWebDriver webDriver, string selectClass, int index)
+        {
+            return new SelectElement(webDriver.FindElement(By.XPath($"(//select[contains(@class, '{selectClass}')])[{index}]")));
+        }
+    }
+}
